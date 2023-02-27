6d424590734352519f5ada3fb938dd07f45ac5ca
Felipe M Ornelas <fe.mariuba@gmail.com>
2023-02-27 16:57:30 -0300

    Primeiro Projeto

---
+++ Primeiro_Java/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		System.out.println("Hello Word " + (a+b));
+
+	}
+
+}
